1cfe528a31dcbec39b8d4e6c0a0592f47531a2d1
djfksdl <djfksdl@naver.com>
2024-03-02 23:55:45 +0900

    spring setting초기화

--- src/main/java/com/javaex/controller/HelloController.java
+++ src/main/java/com/javaex/controller/HelloController.java
@@ -7,5 +7,6 @@ public class HelloController {
 	 public String hello() {
 	 System.out.println("/hellospring/hello");
 	 return "/WEB-INF/views/index.jsp";
+	 // spring-servlet에서 뷰리졸버 쓰고나선 return index;만 쓰면 됨
 	}
 }
